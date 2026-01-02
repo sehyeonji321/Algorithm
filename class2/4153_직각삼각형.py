a1a52ec4cacc7b3ac35ad3774cfa1b59836365ac
SehyeonJi <sehb0321@gmail.com>
2026-01-03 00:34:46 +0900

    First commit!

---
+++ class2/4153_직각삼각형.py
@@ -0,0 +1,12 @@
+while True:
+    a, b, c = map(int, input().split())
+
+    if a == b and b == c and a == 0:
+        break
+
+    a, b, c = sorted((a, b, c))
+
+    if c**2 == a**2 + b**2:
+        print("right")
+    else:
+        print("wrong")
