f06db3250dc2527ed80bcb95f755e30da1b00c9a
SehyeonJi <sehb0321@gmail.com>
2026-03-16 10:28:30 +0900

    260316

--- class2/4949_균형잡힌세상.py
+++ class2/4949_균형잡힌세상.py
@@ -1,33 +1,17 @@
 def check(seq):
     stack = []
+    mapping = {")": "(", "]":"["}
 
     for s in seq:
-        if s == "(":
-            stack.append(0)
-        elif s == "[":
-            stack.append(1)
-        elif s == ")":
-            if len(stack) == 0:
+        if s == "(" or s == "[": # 여는 괄호면 스택에 넣음
+            stack.append(s)
+        elif s == ")" or s == "]": # 닫는 괄호면, pop할 수 없거나, 스택에서 뽑은게 쌍이 안맞으면 False
+            if not stack or stack.pop() != mapping[s]:
                 return False
-            if stack.pop() != 0:
-                return False
-        elif s == "]":
-            if len(stack) == 0:
-                return False
-            if stack.pop() != 1:
-                return False
-    if len(stack) == 0:
-        return True
+    return len(stack) == 0
     
-
-
-
 while True:
     seq = input()
     if seq == ".":
         break
-
-    if check(seq):
-        print("yes")
-    else:
-        print("no")
+    print("yes" if check(seq) else "no")
